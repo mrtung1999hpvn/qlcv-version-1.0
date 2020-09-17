92fe6f1e1eb4cf54b7eff87f528c05a034de3a81
ThanhTung <63633930+mrtung1999hpvn@users.noreply.github.com>
2020-09-18 04:12:20 +0800

    Updates

---
+++ precache-manifest.c88bada6be31fc03dd814aed0eb146f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e1940208dc41a456ada50448e30945",
+    "url": "/qlcv-version-1.0/index.html"
+  },
+  {
+    "revision": "7db795b694f709d35171",
+    "url": "/qlcv-version-1.0/static/css/main.3d7b650e.chunk.css"
+  },
+  {
+    "revision": "786375e1cc57df008d28",
+    "url": "/qlcv-version-1.0/static/js/2.78fc6c6d.chunk.js"
+  },
+  {
+    "revision": "ed0ceb6ce90d1a2edaf31310108c5357",
+    "url": "/qlcv-version-1.0/static/js/2.78fc6c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db795b694f709d35171",
+    "url": "/qlcv-version-1.0/static/js/main.66549c39.chunk.js"
+  },
+  {
+    "revision": "617123f6d8eae7b43247",
+    "url": "/qlcv-version-1.0/static/js/runtime-main.ed9fc6a6.js"
+  },
+  {
+    "revision": "7dbeceaa32a5c88192e3c7e7021e159a",
+    "url": "/qlcv-version-1.0/static/media/Spinner-1s-200px.7dbeceaa.gif"
+  }
+]);
